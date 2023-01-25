1445e52c537f8c522085047b544e747d43b54de4
unknown <grabbany9258>
2023-01-25 19:14:45 +0600

    Query builde & Debugbar Added

--- wdpf51_laravel_pr2/app/Http/Controllers/ReportController.php
+++ wdpf51_laravel_pr2/app/Http/Controllers/ReportController.php
@@ -9,9 +9,66 @@ class ReportController extends Controller
 {
     public function report1()
     {
-        $data =  DB::table('products')->where('product_category', 3)->get();
-        echo $data->count();
+        $data =  DB::table('offices')->where('country', 'USA')->get();
+        // echo $data->count();
+        // echo "<pre>";
+        // print_r($data);
+
+        // return view('reports.report1', compact('data'));
+
+        // ###### Report 2 #####
+        // $data = DB::table('employees')->whereIn('officeCode', [1, 2, 3]);
+        // $result =  $data->get();
+        // echo "<pre>";
+        // print_r($result);
+
+        //     ###### Report 3 #####
+        //     $data = DB::table('employees')->whereIn('officeCode', [1, 2, 3])->where('jobTitle', 'Sales Rep');
+        //     $result =  $data->get();
+        //     echo "<pre>";
+        //     print_r($result);
+
+
+        // ###### Report 4 #####
+        // $data = DB::table('employees')->select(DB::raw('CONCAT(firstName, " ", lastName)AS fullName'), 'email', 'jobTitle');
+        // $result =  $data->get();
+        // echo "<pre>";
+        // print_r($result);
+
+        // ###### Report 5 #####
+        // $data = DB::table('employees')->select(DB::raw('count(employeeNumber)'));
+        // $result =  $data->get();
+        // echo $data->count();
+        // echo "<pre>";
+        // print_r($result);
+
+        // ###### Report 6 #####
+        // $data = DB::table('employees')->select(DB::raw('count(employeeNumber)'), 'jobTitle')->groupBy('jobTitle');
+        // $result =  $data->get();
+        // echo $result->count();
+        // echo "<pre>";
+        // print_r($result);
+
+        // ###### Report 7 #####
+        // $data = DB::table('employees')->where('officeCode',  3)->orwhere('officeCode',  5);
+        // $result =  $data->get();
+        // echo $result->count();
+        // echo "<pre>";
+        // print_r($result);
+
+
+        // ###### Report 8 page:134  ##### 
+        // $data = DB::table('employees')->whereBetween('officeCode', [1, 3]);
+        // $result =  $data->get();
+        // echo "<pre>";
+        // print_r($result);
+
+        ###### Report 9 page:135  ##### 
+        $data = DB::table('employees');
+        $data->join('offices', 'offices.officeCode', '=',  'employees.officeCode');
+        $result =  $data->get();
+        echo $result->count();
         echo "<pre>";
-        print_r($data);
+        print_r($result);
     }
 }
