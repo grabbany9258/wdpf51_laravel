00b9f4d777afe77c7c630ae3592fd3f32b31a18c
unknown <grabbany9258>
2023-02-13 19:01:49 +0600

    api data taken pass with axios

--- wdpf51_laravel_pr2/app/Http/Controllers/Api/SearchProduct.php
+++ wdpf51_laravel_pr2/app/Http/Controllers/Api/SearchProduct.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SearchProduct extends Controller
 {
-    public function search(Request $req){
-        return $req->all();
+    public function search(Request $req)
+    {
+        // return $req->all();
+        $term = $req->item;
+        $result =  DB::table('products')->where('product_name', 'LIKE', "%$term%")->get();
+        return $result;
     }
 }
